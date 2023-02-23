e7ae4a826fbb9d5f4a3d5e80f943d79111e81aa7
mcxzyang <mcxzyang@163.com>
2023-02-23 17:40:39 +0800

    feat: udpate

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -81,8 +81,8 @@
 
   const loginConfig = useStorage('login-config', {
     rememberPassword: true,
-    username: 'admin', // 演示默认值
-    password: '123456', // demo default value
+    username: '', // 演示默认值
+    password: '', // demo default value
   });
   const userInfo = reactive({
     username: loginConfig.value.username,
